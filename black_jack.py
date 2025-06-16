880f945eb5fe8c673d0c0dc894b13b5013235872
ELarga-2206 <earl.larga22@gmail.com>
2025-06-16 21:21:32 +0800

    added more possible game conditions

--- black_jack.py
+++ black_jack.py
@@ -169,3 +169,43 @@ while True:
 
     # Show cards (but keep one dealer card hidden)
     show_some(player_hand,dealer_hand)
+
+    while playing:  # recall this variable from our hit_or_stand function
+
+        # Prompt for Player to Hit or Stand
+        hit_or_stand(deck,player_hand)
+
+        # Show cards (but keep one dealer card hidden)
+        show_some(player_hand,dealer_hand)
+
+        # If player's hand exceeds 21, run player_busts() and break out of loop
+        if player_hand.value > 21:
+            player_busts(player_hand,dealer_hand,player_chips)
+            break
+
+            # If Player hasn't busted, play Dealer's hand until Dealer reaches 17
+    if player_hand.value <= 21:
+
+        while dealer_hand.value < 17:
+            hit(deck,dealer_hand)
+
+            # Show all cards
+        show_all(player_hand,dealer_hand)
+
+        # Run different winning scenarios
+        if dealer_hand.value > 21:
+            dealer_busts(player_hand,dealer_hand,player_chips)
+
+        elif dealer_hand.value > player_hand.value:
+            dealer_wins(player_hand,dealer_hand,player_chips)
+
+        elif dealer_hand.value < player_hand.value:
+            player_wins(player_hand,dealer_hand,player_chips)
+
+        else:
+            push(player_hand,dealer_hand)
+
+            # Inform Player of their chips total
+    print("\nPlayer's winnings stand at",player_chips.total)
+
+
