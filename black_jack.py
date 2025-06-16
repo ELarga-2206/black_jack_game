002adf84f7dd4f6a958f7e70a0e8b13537b37be5
ELarga-2206 <earl.larga22@gmail.com>
2025-06-16 20:57:56 +0800

    added 'deck' class -- to create a deck of cards to build, shuffle, and deal cards

--- black_jack.py
+++ black_jack.py
@@ -22,3 +22,23 @@ class Card:
     def __str__(self):
         return self.rank + ' of ' + self.suit
 
+class Deck:
+
+    def __init__(self):
+        self.deck = []  # start with an empty list
+        for suit in suits:
+            for rank in ranks:
+                self.deck.append(Card(suit,rank))  # build Card objects and add them to the list
+
+    def __str__(self):
+        deck_comp = ''  # start with an empty string
+        for card in self.deck:
+            deck_comp += '\n '+card.__str__() # add each Card object's print string
+        return 'The deck has:' + deck_comp
+
+    def shuffle(self):          # shuffle function will shuffle the whole deck
+        random.shuffle(self.deck)
+
+    def deal(self):             # deal function will take one card from the deck
+        single_card = self.deck.pop()
+        return single_card
