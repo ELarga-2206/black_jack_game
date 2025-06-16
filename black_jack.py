b98509079a16a31a7114c389bd07780afae7f571
ELarga-2206 <earl.larga22@gmail.com>
2025-06-16 21:08:34 +0800

    added functions for taking bets -- 'take_bet' for taking player's bet amount, def 'hit' for taking hits

--- black_jack.py
+++ black_jack.py
@@ -74,3 +74,23 @@ class Chips:
 
     def lose_bet(self):
         self.total -= self.bet
+
+def take_bet(chips):
+
+    while True:
+        try:
+            chips.bet = int(input('How many chips would you like to bet? '))
+        except ValueError:
+            print('Sorry, a bet must be an integer!')
+        else:
+            if chips.bet > chips.total:
+                print("Sorry, your bet can't exceed",chips.total)
+            else:
+                break
+
+# function for taking hits
+
+def hit(deck,hand):
+
+    hand.add_card(deck.deal())
+    hand.adjust_for_ace()
