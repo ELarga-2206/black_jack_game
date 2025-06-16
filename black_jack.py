a486da9c4abca83dc6c071312516f62ba6ceec66
ELarga-2206 <earl.larga22@gmail.com>
2025-06-16 21:03:25 +0800

    added class 'Chips' -- to handle betting chips and functions for winning and losing bets

--- black_jack.py
+++ black_jack.py
@@ -62,3 +62,15 @@ class Hand:
         while self.value > 21 and self.aces:
             self.value -= 10
             self.aces -= 1
+
+class Chips:
+
+    def __init__(self):
+        self.total = 100  # This can be set to a default value or supplied by a user input
+        self.bet = 0
+
+    def win_bet(self):
+        self.total += self.bet
+
+    def lose_bet(self):
+        self.total -= self.bet
